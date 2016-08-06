c584e1865d7c3b07b85b9323778198b8ddba3f37
Paul van Zyl <paul@nonacreative.com>
2016-08-06 22:38:16 +0200

    cleanup and rename contextName to context

--- helpers.js
+++ helpers.js
@@ -1,16 +1,15 @@
 /* Setup the form errors */
 Template.offensiveForm.onCreated(() => {
-  var instance = Template.instance();
-  // var collection = Offensive.clientCollectionLookup(self.data.collection);
-  const { collection, contextName } = instance.data;
+  const instance = Template.instance();
+  const { collection, context } = instance.data;
   const collectionInstance = Mongo.Collection.get(collection);
-  const context = collectionInstance.simpleSchema().namedContext(contextName);
+  const validation = collectionInstance.simpleSchema().namedContext(context);
   let key;
 
   /* abort if no collection is specified */
   if (!collectionInstance) {
-    console.log('You need to add a collection argument to the form template');
-    return false;
+    console.error('You need to add a collection argument to the form template');
+    return;
   }
 
 
@@ -20,9 +19,9 @@ Template.offensiveForm.onCreated(() => {
     Offensive.resetErrors();
 
     /* Set the errors messsages on the errors object */
-    context.invalidKeys().map((data) => {
-      key = `${contextName}_${data.name}`;
-      Offensive.errors.set(key, context.keyErrorMessage(data.name));
+    validation.invalidKeys().map((data) => {
+      key = `${context}_${data.name}`;
+      Offensive.errors.set(key, validation.keyErrorMessage(data.name));
     });
   });
 });
@@ -30,7 +29,8 @@ Template.offensiveForm.onCreated(() => {
 /* Template Form Errors Primary Template */
 Template.offensiveForm.helpers({
   showErrorList() {
-    return this.list ? this.list : Offensive._settings.showButtons || false;
+    const { list } = this;
+    return list ? list : Offensive._settings.showButtons || false;
   },
   errors() {
     return _.keys(Offensive.errors.all()) || [];
@@ -40,7 +40,7 @@ Template.offensiveForm.helpers({
 /* the single errors that are show in the listing */
 Template.offensiveSingle.helpers({
   message() {
-    if (this.key.indexOf(this.contextName) === -1) {
+    if (this.key.indexOf(this.context) === -1) {
       return false;
     }
 
@@ -63,8 +63,8 @@ Template.offensiveSingle.events({
 /* The field error template  */
 Template.offensiveField.helpers({
   message() {
-    context = this.contextName ? this.contextName + '_' : '';
-    return this.field ? Offensive.errors.get(context + this.field) : false;
+    const { context = '', field = '' } = this;
+    return field ? Offensive.errors.get(`${context}_${field}`) : false;
   },
   showButtons() {
     return Offensive._settings.showButtons;
@@ -76,14 +76,13 @@ Template.offensiveField.helpers({
 
 Template.offensiveField.events({
   'click .remove'(event, template) {
-    context = this.contextName ? this.contextName + '_' : '';
-    Offensive.errors.delete(context + this.field);
+    const { context = '', field } = this;
+    Offensive.errors.delete(`${context}_${field}`);
   }
 });
 
 /* a global helper to apply an error class to the offending fields */
 Template.registerHelper('offensiveClass', (key) => {
-  var field = key ? key.hash && key.hash.field : '';
-  var context = key ? key.hash && key.hash.contextName + '_' : '';
-  return Offensive.errors.get(context + field) && 'error clarity-error';
+  const { field = '', context = '' } = key.hash;
+  return Offensive.errors.get(`${context}_${field}`) && 'error offensive-error';
 });
--- offensive.js
+++ offensive.js
@@ -17,31 +17,5 @@ Offensive = {
   resetErrors: function() {
     Offensive.errors.clear();
   },
-
-  /* Utilities */
-
-  /*
-    This is a very clever little function care of autoForms author aldeed,
-    and can be seen in use here:
-
-    https://github.com/aldeed/meteor-autoform/blob/devel/utility.js
-
-    If `obj` is a string, returns the value of the property with that
-    name on the `window` object. Otherwise returns `obj`.  For our client side
-    validation, the collection must exist be isoMorphic.
-   */
-  clientCollectionLookup: function(obj) {
-    var ref = window;
-    var arr;
-    if (typeof obj === 'string') {
-      arr = obj.split('.');
-      while (arr.length && (ref = ref[arr.shift()]));
-      if (!ref) {
-        throw new Error(obj + ' is not in the window scope');
-      }
-      return ref;
-    }
-    return obj;
-  }
 };
 
